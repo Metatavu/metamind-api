06e9fdc4aae75f42a9c57a7f1fae36f6af7315d6
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-11-29 00:55:20 +0200

    Improved bot accuracy

--- src/main/java/fi/metatavu/metamind/bot/MetamindBotConfiguration.java
+++ src/main/java/fi/metatavu/metamind/bot/MetamindBotConfiguration.java
@@ -9,6 +9,7 @@ import com.rabidgremlin.mutters.bot.ink.InkBotConfiguration;
 import com.rabidgremlin.mutters.bot.ink.InkBotFunction;
 import com.rabidgremlin.mutters.bot.ink.SessionUtils;
 import com.rabidgremlin.mutters.bot.ink.StoryUtils;
+import com.rabidgremlin.mutters.core.CompoundIntentMatcher;
 import com.rabidgremlin.mutters.core.Intent;
 import com.rabidgremlin.mutters.core.IntentMatch;
 import com.rabidgremlin.mutters.core.IntentMatcher;
@@ -16,7 +17,11 @@ import com.rabidgremlin.mutters.core.session.Session;
 import com.rabidgremlin.mutters.opennlp.intent.OpenNLPIntentMatcher;
 import com.rabidgremlin.mutters.opennlp.intent.OpenNLPTokenizer;
 import com.rabidgremlin.mutters.opennlp.ner.OpenNLPSlotMatcher;
+import com.rabidgremlin.mutters.slots.LiteralSlot;
 import com.rabidgremlin.mutters.slots.NumberSlot;
+import com.rabidgremlin.mutters.templated.SimpleTokenizer;
+import com.rabidgremlin.mutters.templated.TemplatedIntent;
+import com.rabidgremlin.mutters.templated.TemplatedIntentMatcher;
 
 import opennlp.tools.tokenize.WhitespaceTokenizer;
 
@@ -24,54 +29,79 @@ public class MetamindBotConfiguration implements InkBotConfiguration {
   
   @Override
   public IntentMatcher getIntentMatcher() {
-    // model was built with OpenNLP whitespace tokenizer
+
+    TemplatedIntentMatcher templatedIntentMatcher = new TemplatedIntentMatcher(new SimpleTokenizer());
+
+    TemplatedIntent templatedElectricyComsumptionIntent = templatedIntentMatcher.addIntent("GaveElectricityConsumption");
+    templatedElectricyComsumptionIntent.addUtterance("{ElectricityConsumption}");
+    templatedElectricyComsumptionIntent.addUtterance("{ElectricityConsumption} kwh");
+    templatedElectricyComsumptionIntent.addUtterance("{ElectricityConsumption}kwh");
+    templatedElectricyComsumptionIntent.addUtterance("{ElectricityConsumption} kilowattituntia");
+    templatedElectricyComsumptionIntent.addSlot(new NumberSlot("ElectricityConsumption"));
+    
+    TemplatedIntent templatedResidentCountIntent = templatedIntentMatcher.addIntent("GaveResidentCount");
+    templatedResidentCountIntent.addUtterance("{ResidentCount}");
+    templatedResidentCountIntent.addUtterance("{ResidentCount} henkilöä");
+    templatedResidentCountIntent.addUtterance("{ResidentCount} hlö");
+    templatedResidentCountIntent.addUtterance("{ResidentCount} hlöä");
+    templatedResidentCountIntent.addSlot(new NumberSlot("ResidentCount"));
+    
+    TemplatedIntent templatedLivingSpaceIntent = templatedIntentMatcher.addIntent("GaveLivingSpaceSize");
+    templatedLivingSpaceIntent.addUtterance("{LivingSpaceSize}");
+    templatedLivingSpaceIntent.addUtterance("{LivingSpaceSize} neliö");
+    templatedLivingSpaceIntent.addUtterance("{LivingSpaceSize} neliömetriä");
+    templatedLivingSpaceIntent.addUtterance("{LivingSpaceSize} m2");
+    templatedLivingSpaceIntent.addSlot(new NumberSlot("LivingSpaceSize"));
+
     OpenNLPTokenizer tokenizer = new OpenNLPTokenizer(WhitespaceTokenizer.INSTANCE);
 
-    // use OpenNLP NER for slot matching
     OpenNLPSlotMatcher slotMatcher = new OpenNLPSlotMatcher(tokenizer);
     slotMatcher.addSlotModel("ElectricityConsumption", "models/fi-ner-elecricity-consumption.bin");
     slotMatcher.addSlotModel("LivingSpaceSize", "models/fi-ner-living-space-size.bin");
     slotMatcher.addSlotModel("ResidentCount", "models/fi-ner-resident-count.bin");
     slotMatcher.addSlotModel("YearBuild", "models/fi-ner-year-build.bin");
-    
-    // create intent matcher
-    OpenNLPIntentMatcher matcher = new OpenNLPIntentMatcher("models/fi-lumme-intents.bin", tokenizer, slotMatcher);
+    slotMatcher.addSlotModel("PhoneNumber", "models/fi-ner-phone-number.bin");
+    OpenNLPIntentMatcher machineLearningIntentMatcher = new OpenNLPIntentMatcher("models/fi-lumme-intents.bin", tokenizer, slotMatcher);
 
     Intent orderElectricityIntent = new Intent("OrderElectricity");
-    matcher.addIntent(orderElectricityIntent);
+    machineLearningIntentMatcher.addIntent(orderElectricityIntent);
 
     Intent yesIntent = new Intent("YesIntent");
-    matcher.addIntent(yesIntent);
+    machineLearningIntentMatcher.addIntent(yesIntent);
     
     Intent noIntent = new Intent("NoIntent");
-    matcher.addIntent(noIntent);
+    machineLearningIntentMatcher.addIntent(noIntent);
     
     Intent gaveElectricyComsumptionIntent = new Intent("GaveElectricityConsumption");
     gaveElectricyComsumptionIntent.addSlot(new NumberSlot("ElectricityConsumption"));
-    matcher.addIntent(gaveElectricyComsumptionIntent);
+    machineLearningIntentMatcher.addIntent(gaveElectricyComsumptionIntent);
     
     Intent houseIntent = new Intent("HouseIntent");
-    matcher.addIntent(houseIntent);
+    machineLearningIntentMatcher.addIntent(houseIntent);
     
     Intent rowhouseIntent = new Intent("RowhouseIntent");
-    matcher.addIntent(rowhouseIntent);
+    machineLearningIntentMatcher.addIntent(rowhouseIntent);
     
     Intent apartmentHouseIntent = new Intent("ApartmentHouseIntent");
-    matcher.addIntent(apartmentHouseIntent);
+    machineLearningIntentMatcher.addIntent(apartmentHouseIntent);
     
     Intent gaveResidentCountIntent = new Intent("GaveResidentCount");
     gaveResidentCountIntent.addSlot(new NumberSlot("ResidentCount"));
-    matcher.addIntent(gaveResidentCountIntent);
+    machineLearningIntentMatcher.addIntent(gaveResidentCountIntent);
     
     Intent gaveLivingSpaceSizeIntent = new Intent("GaveLivingSpaceSize");
     gaveLivingSpaceSizeIntent.addSlot(new NumberSlot("LivingSpaceSize"));
-    matcher.addIntent(gaveLivingSpaceSizeIntent);
+    machineLearningIntentMatcher.addIntent(gaveLivingSpaceSizeIntent);
     
     Intent gaveYearBuildIntent = new Intent("GaveYearBuild");
     gaveYearBuildIntent.addSlot(new NumberSlot("YearBuild"));
-    matcher.addIntent(gaveYearBuildIntent);
+    machineLearningIntentMatcher.addIntent(gaveYearBuildIntent);
     
-    return matcher;
+    Intent gavePhoneNumberIntent = new Intent("GavePhoneNumber");
+    gavePhoneNumberIntent.addSlot(new LiteralSlot("PhoneNumber"));
+    machineLearningIntentMatcher.addIntent(gavePhoneNumberIntent);
+    
+    return new CompoundIntentMatcher(templatedIntentMatcher, machineLearningIntentMatcher);
   }
 
   @Override
@@ -111,6 +141,25 @@ public class MetamindBotConfiguration implements InkBotConfiguration {
       }
 
     });
+    
+    functions.add(new InkBotFunction() {
+      @Override
+      public void execute(CurrentResponse currentResponse, Session session, IntentMatch intentMatch, Story story,
+          String param) {
+        try {
+          String phoneNumber = SessionUtils.getStringFromSlotOrSession(intentMatch, session, "phoneNumber", null);
+          System.out.println(phoneNumber);
+        } catch (Exception e) {
+          throw new RuntimeException("Unable to get phonenumber", e);
+        }
+      }
+
+      @Override
+      public String getFunctionName() {
+        return "SEND_CONTACT_REQUEST";
+      }
+
+    });
 
     return functions;
   }
@@ -124,8 +173,7 @@ public class MetamindBotConfiguration implements InkBotConfiguration {
 
   @Override
   public ConfusedKnot getConfusedKnot() {
-    // TODO Auto-generated method stub
-    return null;
+    return new ConfusedKnot(3, "bail_out");
   }
 
   @Override
