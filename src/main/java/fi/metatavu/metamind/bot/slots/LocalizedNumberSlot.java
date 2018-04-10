24a92be4bf501d406cf29e0c9ba3a6ce867350ef
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-04-10 08:44:21 +0300

    Refactored finnish written number recognition

--- src/main/java/fi/metatavu/metamind/bot/slots/LocalizedNumberSlot.java
+++ src/main/java/fi/metatavu/metamind/bot/slots/LocalizedNumberSlot.java
@@ -17,6 +17,8 @@ public class LocalizedNumberSlot extends NumberSlot {
     "english"
   };
   
+  private static final int MAX_TOKENS = 100;
+  
   public LocalizedNumberSlot(String name) {
     super(name);
   }
@@ -76,123 +78,92 @@ public class LocalizedNumberSlot extends NumberSlot {
       return null;
     }
 
-    token = token.replaceAll("-", " ");
-    token = token.replaceAll(",", " ");
-    token = token.toLowerCase().replaceAll(" ja", " ");
-    String[] splittedParts = token.trim().split("\\s+");
+    token = token.toLowerCase().replaceAll("[\\s+\\-,]", "").trim();
 
     long result = 0;
+    long finalResult = 0;
+    int tokenCount = 0;
+    
+    while (token.length() > 0 && tokenCount < MAX_TOKENS) {
+      tokenCount++;
+      String foundToken = null;
 
-    for (String str : splittedParts) {
-      str = str.toLowerCase();
-      switch (str) {
-        case "nolla":
-          result += 0;
-        break;
-        case "yksi":
-          result += 1;
-        break;
-        case "kaksi":
-          result += 2;
-        break;
-        case "kolme":
-          result += 3;
-        break;
-        case "neljä":
-          result += 4;
-        break;
-        case "viisi":
-          result += 5;
-        break;
-        case "kuusi":
-          result += 6;
-        break;
-        case "seitsemän":
-          result += 7;
-        break;
-        case "kahdeksan":
-          result += 8;
-        break;
-        case "yhdeksän":
-          result += 9;
-        break;
-        case "kymmenen":
-          result += 10;
-        break; 
-        case "yksitoista":
-          result += 11;
-        break; 
-        case "kaksitoista":
-          result += 12;
-        break;
-        case "kolmetoista":
-          result += 13;
-        break;
-        case "neljätoista":
-          result += 14;
-        break;
-        case "viisitoista":
-          result += 15;
-        break;
-        case "kuusitoista":
-          result += 16;
-        break;
-        case "seitsemäntoista":
-          result += 17;
-        break;
-        case "kahdeksantoista":
-          result += 18;
-        break;
-        case "yhdeksäntoista":
-          result += 19;
-        break;
-        case "kaksikymmentä":
-          result += 20;
-        break;
-        case "kolmekymmentä":
-          result += 30;
-        break;
-        case "neljäkymmentä":
-          result += 40;
-        break;
-        case "viisikymmentä":
-          result += 50;
-        break;
-        case "kuusikymmentä":
-          result += 60;
-        break;
-        case "seitsemänkymmentä":
-          result += 70;
-        break;
-        case "kahdeksankymmentä":
-          result += 80;
-        break;
-        case "yhdeksänkymmentä":
-          result += 90;
-        break;
-        case "sata":
-          result += 100;
-        case "sataa":
-          result *= 100;
-        break;
-        case "tuhat":
-          result += 1000;
-        break;
-        case "tuhatta":
-          result *= 1000;
-        break;
-        case "miljoona":
-          result += 1000000;
-        break;
-        case "miljoonaa":
-          result *= 1000000;
-        break;
-        default:
-          return null;
+      if (token.startsWith("miljoonaa")) {
+        result *= 1000000;
+        finalResult += result;
+        result = 0;
+        foundToken = "miljoonaa";
+      } else if (token.startsWith("miljoona")) {
+        finalResult += 1000000;
+        result = 0;
+        foundToken = "miljoona";
+      } else if (token.startsWith("tuhatta")) {
+        result *= 1000;
+        finalResult += result;
+        result = 0;
+        foundToken = "tuhatta";
+      } else if (token.startsWith("tuhat")) {
+        result += 1000;
+        foundToken = "tuhat";
+      } else if (token.startsWith("sataa")) {
+        result *= 100;
+        finalResult += result;
+        result = 0;
+        foundToken = "sataa";
+      } else if (token.startsWith("sata")) {
+        result += 100;
+        foundToken = "sata";
+      } else if (token.startsWith("kymmentä")) {
+        result *= 10;
+        finalResult += result;
+        result = 0;
+        foundToken = "kymmentä";
+      } else if (token.startsWith("toista")) {
+        result += 10;
+        foundToken = "toista";
+      } else if (token.startsWith("kymmenen")) {
+        result += 10;
+        foundToken = "kymmenen";
+      } else if (token.startsWith("yhdeksän")) {
+        result += 9;
+        foundToken = "yhdeksän";
+      } else if (token.startsWith("kahdeksan")) {
+        result += 8;
+        foundToken = "kahdeksan";
+      } else if (token.startsWith("seitsemän")) {
+        result += 7;
+        foundToken = "seitsemän";
+      } else if (token.startsWith("kuusi")) {
+        result += 6;
+        foundToken = "kuusi";
+      } else if (token.startsWith("viisi")) {
+        result += 5;
+        foundToken = "viisi";
+      } else if (token.startsWith("neljä")) {
+        result += 4;
+        foundToken = "neljä";
+      } else if (token.startsWith("kolme")) {
+        result += 3;
+        foundToken = "kolme";
+      } else if (token.startsWith("kaksi")) {
+        result += 2;
+        foundToken = "kaksi";
+      } else if (token.startsWith("yksi")) {
+        result += 1;
+        foundToken = "yksi";
+      } else if (token.startsWith("nolla")) {
+        result += 0;
+        foundToken = "nolla";
+      } else {
+        return null;
       }
+      
+      token = token.replaceFirst(foundToken, "");
     }
-
-    return result;
+    
+    finalResult += result;
+    result = 0;
+    return finalResult;
   }
 
 }
