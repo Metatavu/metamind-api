abdb2704e13cd38fbc87cbb524ce7c7d136a015d
Antti Leppä <antti.leppa@metatavu.fi>
2018-10-01 15:03:12 +0300

    Initial draft of beer list bot function

---
+++ src/main/java/fi/metatavu/metamind/bot/functions/BeerApiListBeersMetaBotFunction.java
@@ -0,0 +1,88 @@
+package fi.metatavu.metamind.bot.functions;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.Map;
+
+import com.bladecoder.ink.runtime.Story;
+import com.rabidgremlin.mutters.bot.ink.CurrentResponse;
+import com.rabidgremlin.mutters.bot.ink.functions.FunctionDetails;
+import com.rabidgremlin.mutters.bot.ink.functions.FunctionHelper;
+import com.rabidgremlin.mutters.core.IntentMatch;
+import com.rabidgremlin.mutters.core.session.Session;
+
+import fi.metatavu.beer.ApiClient;
+import fi.metatavu.beer.client.BarBeersApi;
+import fi.metatavu.beer.client.Beer;
+import fi.metatavu.metamind.story.StoryController;
+
+/**
+ * Metabot function to list beers from Beer API
+ * 
+ * @author Antti Leppä
+ */
+@ApplicationScoped
+public class BeerApiListBeersMetaBotFunction extends AbstractMetaBotFunction {
+
+  @Inject
+  private Logger logger;
+
+  @Inject
+  private StoryController storyController;
+
+  @Override
+  public String getFunctionName() {
+    return "BEER_API_LIST_BEERS";
+  }
+
+  @Override
+  public void execute(CurrentResponse currentResponse, Session session, IntentMatch intentMatch, Story story, String param) {
+    FunctionDetails details = FunctionHelper.parseFunctionString(param);
+
+    if (details == null) {
+      logger.error("Function parameters is null at BEER_API_LIST_BEERS");
+      return;
+    }
+    
+    Map<String, String> functionParams = details.getFunctionParams();
+    String variableName = functionParams.get("variable");
+    String barId = functionParams.get("barId");
+    String searchFlavors = functionParams.get("searchFlavors");
+    List<String> sort = null;
+    Long firstResult = NumberUtils.createLong(functionParams.get("firstResult"));
+    Long maxResults = NumberUtils.createLong(functionParams.get("maxResults"));
+    
+    ApiClient apiClient = getApiClient(getMetamindStory(session));
+    BarBeersApi barBeersApi = apiClient.buildClient(BarBeersApi.class);
+    List<Beer> beers = barBeersApi.listBeers(barId, searchFlavors, sort, firstResult, maxResults);
+    
+    try {
+      story.getVariablesState().set(variableName, beers);
+    } catch (Exception e) {
+      logger.error("Could not set variable state", e);
+      return;
+    }
+  }
+  
+  /**
+   * Returns Unauthorized API client for Beer API
+   * @param metamindStory 
+   * 
+   * @return Unauthorized API client for Beer API
+   */
+  private ApiClient getApiClient(fi.metatavu.metamind.persistence.models.Story metamindStory) {
+    ApiClient apiClient = new ApiClient();
+
+    String host = storyController.getGlobalStoryVariable(metamindStory, "beer-api-host");
+    Integer port = NumberUtils.createInteger(storyController.getGlobalStoryVariable(metamindStory, "beer-api-port"));
+
+    String basePath = String.format("http://%s:%d/v1", host, port);
+    apiClient.setBasePath(basePath);
+    return apiClient;
+  }
+}
