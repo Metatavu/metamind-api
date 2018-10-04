81a9d41bcfe142fad14da13d1d96fee9025d8379
Antti Leppä <antti.leppa@metatavu.fi>
2018-10-04 13:59:41 +0300

    Beer list

--- src/main/java/fi/metatavu/metamind/bot/functions/BeerApiListBeersMetaBotFunction.java
+++ src/main/java/fi/metatavu/metamind/bot/functions/BeerApiListBeersMetaBotFunction.java
@@ -1,5 +1,6 @@
 package fi.metatavu.metamind.bot.functions;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,19 +8,19 @@ import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.slf4j.Logger;
+
 import com.bladecoder.ink.runtime.Story;
 import com.rabidgremlin.mutters.bot.ink.CurrentResponse;
 import com.rabidgremlin.mutters.core.IntentMatch;
 import com.rabidgremlin.mutters.core.session.Session;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
-import org.slf4j.Logger;
-
 import fi.metatavu.beer.ApiClient;
 import fi.metatavu.beer.client.BarBeersApi;
-import fi.metatavu.beer.client.Beer;
 import fi.metatavu.beer.client.BarBeersApi.ListBeersQueryParams;
+import fi.metatavu.beer.client.Beer;
 import fi.metatavu.metamind.story.StoryController;
 
 /**
@@ -46,19 +47,52 @@ public class BeerApiListBeersMetaBotFunction extends AbstractMetaBotFunction {
     Map<String, String> functionParams = getParsedParam(param);
     String variableName = functionParams.get("variable");
     String barId = functionParams.get("barId");
-    String preferFlavorsVariable = functionParams.get("preferFlavors");
-    String preferFlavors = getVariableString(story, preferFlavorsVariable);
+    String includeFlavors = getCommaDelimetedVariable(story, functionParams.get("includeFlavors"));
+    String excludeFlavors = getCommaDelimetedVariable(story, functionParams.get("excludeFlavors"));
+    String preferFlavors = getCommaDelimetedVariable(story, functionParams.get("preferFlavors"));
     Float minAbv = getVariableFloat(story, "minAbv");
     Float maxAbv = getVariableFloat(story, "maxAbv");
-    List<String> sort = null;
+    String sort = getVariableString(story, "sort");
     Long firstResult = NumberUtils.createLong(functionParams.get("firstResult"));
     Long maxResults = NumberUtils.createLong(functionParams.get("maxResults"));
     
     ApiClient apiClient = getApiClient(getMetamindStory(session));
     BarBeersApi barBeersApi = apiClient.buildClient(BarBeersApi.class);
     ListBeersQueryParams params = new ListBeersQueryParams();
-    params.preferFlavors(StringUtils.stripStart(preferFlavors, ",").str)
-    List<Beer> beers = barBeersApi.listBeers(barId, searchFlavors, sort, firstResult, maxResults);
+
+    if (excludeFlavors != null) {
+      params.excludeFlavors(excludeFlavors);
+    }
+
+    if (includeFlavors != null) {
+      params.excludeFlavors(includeFlavors);
+    }
+
+    if (preferFlavors != null) {
+      params.excludeFlavors(preferFlavors);
+    }
+    
+    if (maxAbv != null) {
+      params.maxAbv(maxAbv);
+    }
+    
+    if (minAbv != null) {
+      params.maxAbv(minAbv);
+    }
+    
+    if (sort != null) {
+      params.sort(Collections.singletonList(sort));
+    }
+    
+    if (firstResult != null) {
+      params.firstResult(firstResult);
+    }
+    
+    if (maxResults != null) {
+      params.firstResult(maxResults);
+    }
+    
+    List<Beer> beers = barBeersApi.listBeers(barId, params);
     
     try {
       story.getVariablesState().set(variableName, beers.stream().map(beer -> String.valueOf(beer.getId())).collect(Collectors.joining("|")));
@@ -85,7 +119,15 @@ public class BeerApiListBeersMetaBotFunction extends AbstractMetaBotFunction {
     return apiClient;
   }
 
-  private String getCommaDelimetedVariable(String variableName) {
-
+  /**
+   * Returns trimmed value for a comma delimited variable
+   * 
+   * @param story story
+   * @param variableName variableName
+   * @return trimmed value for a comma delimited variable
+   */
+  private String getCommaDelimetedVariable(Story story, String variableName) {
+    String result = getVariableString(story, variableName);
+    return StringUtils.trimToNull(StringUtils.stripEnd(StringUtils.stripStart(result, ","), ","));
   }
 }
