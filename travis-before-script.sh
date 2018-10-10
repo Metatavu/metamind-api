d0a2eaf803424b0d1ab59cf0adb0581bfa127cc9
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-10-10 23:38:41 +0300

    Fixed build

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f sonar-scanner/bin/sonar-scanner ]; then
   rm -fR sonar-scanner
-  wget "https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.0.3.778-linux.zip"
-  unzip sonar-scanner-cli-3.0.3.778-linux.zip
-  mv sonar-scanner-3.0.3.778-linux sonar-scanner
+  wget "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.2.0.1227-linux.zip"
+  unzip sonar-scanner-cli-3.2.0.1227-linux.zip
+  mv sonar-scanner-3.2.0.1227-linux sonar-scanner
 fi;
