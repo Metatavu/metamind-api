cc4b5999e896a18d5f0ba3ba836c87dba99bb9e0
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-10-26 08:46:30 +0300

    Review chnages

--- src/main/java/fi/metatavu/metamind/bot/functions/GetIntegerFromStringMetaBotFunction.java
+++ src/main/java/fi/metatavu/metamind/bot/functions/GetIntegerFromStringMetaBotFunction.java
@@ -1,16 +1,16 @@
 package fi.metatavu.metamind.bot.functions;
 
+import java.util.Map;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
-import org.slf4j.Logger;
-
 import com.bladecoder.ink.runtime.Story;
 import com.rabidgremlin.mutters.bot.ink.CurrentResponse;
-import com.rabidgremlin.mutters.bot.ink.functions.FunctionDetails;
-import com.rabidgremlin.mutters.bot.ink.functions.FunctionHelper;
 import com.rabidgremlin.mutters.core.IntentMatch;
 import com.rabidgremlin.mutters.core.session.Session;
+
+import org.slf4j.Logger;
 
 /**
  * Metabot function to find integers from string
@@ -28,25 +28,17 @@ public class GetIntegerFromStringMetaBotFunction extends AbstractMetaBotFunction
   }
 
   @Override
-  public void execute(CurrentResponse currentResponse, Session session, IntentMatch intentMatch, Story story,
-      String param) {
-   
-   FunctionDetails details = FunctionHelper.parseFunctionString(param);
-   
-   if (details == null) {
-     logger.error("Function parameters is null at GET_INTEGER_FROM_STRING");
-     return;
-   }
-   
-   String stringParam = details.getFunctionParams().get("string");
-   String variableName = details.getFunctionParams().get("var");
-   int weightInt = Integer.parseInt(stringParam.replaceAll("\\D+",""));
-   
-   try {
-     story.getVariablesState().set(variableName, weightInt);
-   } catch (Exception e) {
-     logger.error("Error while setting variables at KUNTA_API_GET_EVENTS", e);
-   }
-
+  public void execute(CurrentResponse currentResponse, Session session, IntentMatch intentMatch, Story story, String param) {
+    Map<String, String> functionParams = getParsedParam(param);
+    
+    String stringParam = functionParams.get("string");
+    String targetVariableName = functionParams.get("target");
+    int result = Integer.parseInt(stringParam.replaceAll("\\D+",""));
+    
+    try {
+      story.getVariablesState().set(targetVariableName, result);
+    } catch (Exception e) {
+      logger.error("Error while getting integer from string", e);
+    }
   }
 }
