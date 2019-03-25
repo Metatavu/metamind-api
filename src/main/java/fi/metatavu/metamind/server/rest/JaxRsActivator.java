20057c14d069eb9e2b642d4d60c2bafcceeb1c29
Antti Leppä <antti.leppa@metatavu.fi>
2019-03-25 11:58:21 +0200

    Removed old version

--- src/main/java/fi/metatavu/metamind/server/rest/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package fi.metatavu.metamind.server.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/v1")
-public class JaxRsActivator extends Application {
-  
-}
