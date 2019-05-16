7dde8bd6896b0887ee0c07b1a20ceb2032009bb1
DaniilMass <daniil.kings@gmail.com>
2019-05-16 11:26:55 +0300

    Imported Knots model

--- src/test/java/fi/metatavu/metamind/test/functional/builder/auth/AbstractTestBuilderAuthentication.java
+++ src/test/java/fi/metatavu/metamind/test/functional/builder/auth/AbstractTestBuilderAuthentication.java
@@ -3,7 +3,9 @@ package fi.metatavu.metamind.test.functional.builder.auth;
 import java.io.IOException;
 
 import fi.metatavu.metamind.ApiClient;
+import fi.metatavu.metamind.client.model.Knot;
 import fi.metatavu.metamind.test.functional.builder.TestBuilder;
+import fi.metatavu.metamind.test.functional.builder.impl.KnotTestBuilderResource;
 import fi.metatavu.metamind.test.functional.builder.impl.StoryTestBuilderResource;
 
 /**
@@ -15,6 +17,7 @@ public abstract class AbstractTestBuilderAuthentication {
 
   private TestBuilder testBuilder;
   private StoryTestBuilderResource stories;
+  private KnotTestBuilderResource knots;
   
   protected AbstractTestBuilderAuthentication(TestBuilder testBuilder) {
     this.testBuilder = testBuilder;
@@ -32,6 +35,21 @@ public abstract class AbstractTestBuilderAuthentication {
     }
     
     return new StoryTestBuilderResource(testBuilder, createClient());
+  }
+  
+  /**
+   * Added by Daniil for knots
+   * 
+   * @return
+   * @throws IOException
+   */
+  
+  public KnotTestBuilderResource knots() throws IOException {
+    if (knots != null) {
+      return knots;
+    }
+    
+    return new KnotTestBuilderResource(testBuilder, createClient());
   }
   
   /**
