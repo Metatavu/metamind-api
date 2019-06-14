64f0247af38849a0a1df48a349692c8c734f8ec4
Simeon Platonov <41298320+platonovsimeon@users.noreply.github.com>
2019-06-14 11:02:16 +0300

    Update WekaController.java

--- src/main/java/fi/metatavu/metamind/weka/WekaController.java
+++ src/main/java/fi/metatavu/metamind/weka/WekaController.java
@@ -16,52 +16,48 @@ import weka.core.Instance;
  */
 public class WekaController {
   private static final Logger LOGGER = Logger.getLogger( WekaController.class.getName() );
-	public WekaController() {
-		
-	}
-	/**
-	 * Returns item iDs for unrated items, sorted by the recommendation algorithm
-	 * 
-	 * @param items items
-	 * @return recommendations recommendations
-	 */
-	
-	public String[] getRecommendations(WekaRecommendationItem[] items) {
-		RecommendationDataUtils dataUtils = new RecommendationDataUtils(items);
-		dataUtils.convertAttributes();
-		dataUtils.splitData();
-		dataUtils.createAttributeInfo();
-		dataUtils.createDatasets();
-		LinearRegression model = new LinearRegression();
-		try {
-			model.buildClassifier(dataUtils.trainingSet);
-		} catch (Exception e) {
-		  LOGGER.log(Level.SEVERE,e.toString(), e);
-		}
-		ArrayList<WekaRecommendationItem> recommendedItems = new ArrayList<WekaRecommendationItem>();
-		for(int i = 0;i<dataUtils.recommendationSet.size();i++) {
-			try {
-				double estimatedRating = recommend(dataUtils.recommendationSet.get(i),model);
-				WekaRecommendationItem item = dataUtils.itemsToRecommend.get(i);
-				item.setRating(estimatedRating);
-				recommendedItems.add(item);
-			} catch (Exception e) {
-					LOGGER.log(Level.SEVERE,e.toString(), e);
-			}
-		}
-		Collections.sort(recommendedItems);
-		String[] ids = new String[recommendedItems.size()];
-		for ( int i = 0; i < recommendedItems.size(); i++ ) {
-			ids[i]=recommendedItems.get(i).getId();
-		}
-		
-		return ids;
-	}
-	
-	double recommend(Object instanceItem,LinearRegression model) throws Exception {
-		Instance instance = (Instance) instanceItem;
-		return model.classifyInstance(instance);
-	}
+  public WekaController() {
+  }
+  /**
+   * Returns item iDs for unrated items, sorted by the recommendation algorithm
+   * 
+   * @param items items
+   * @return recommendations recommendations
+   */
+  public String[] getRecommendations(WekaRecommendationItem[] items) {
+    RecommendationDataUtils dataUtils = new RecommendationDataUtils(items);
+    dataUtils.convertAttributes();
+    dataUtils.splitData();
+    dataUtils.createAttributeInfo();
+    dataUtils.createDatasets();
+    LinearRegression model = new LinearRegression();
+    try {
+      model.buildClassifier(dataUtils.trainingSet);
+    } catch (Exception e) {
+      LOGGER.log(Level.SEVERE,e.toString(), e);
+    }
+    ArrayList<WekaRecommendationItem> recommendedItems = new ArrayList<WekaRecommendationItem>();
+    for(int i = 0;i<dataUtils.recommendationSet.size();i++) {
+      try {
+        double estimatedRating = recommend(dataUtils.recommendationSet.get(i),model);
+        WekaRecommendationItem item = dataUtils.itemsToRecommend.get(i);
+        item.setRating(estimatedRating);
+        recommendedItems.add(item);
+      } catch (Exception e) {
+        LOGGER.log(Level.SEVERE,e.toString(), e);
+      }
+    }
+    Collections.sort(recommendedItems);
+    String[] ids = new String[recommendedItems.size()];
+    for ( int i = 0; i < recommendedItems.size(); i++ ) {
+      ids[i]=recommendedItems.get(i).getId();
+    }
+    return ids;
+  }
+  double recommend(Object instanceItem,LinearRegression model) throws Exception {
+    Instance instance = (Instance) instanceItem;
+    return model.classifyInstance(instance);
+  }
 	
 	
 	
