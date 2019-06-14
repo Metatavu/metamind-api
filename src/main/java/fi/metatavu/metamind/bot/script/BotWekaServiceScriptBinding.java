23bbbf9e99c5bf09861dfb12c88bd33b9ac7fe70
Simeon Platonov <41298320+platonovsimeon@users.noreply.github.com>
2019-06-14 10:40:05 +0300

    Update BotWekaServiceScriptBinding.java

--- src/main/java/fi/metatavu/metamind/bot/script/BotWekaServiceScriptBinding.java
+++ src/main/java/fi/metatavu/metamind/bot/script/BotWekaServiceScriptBinding.java
@@ -26,49 +26,46 @@ public class BotWekaServiceScriptBinding {
   private WekaController wekaController;
 	
   /**
-	 * Returns item iDs for unrated items, sorted by the recommendation algorithm
-	 * 
-	 * @param items items
-	 * @return recommendations
-	 * @throws IOException 
-	 * @throws JsonProcessingException 
-	 * @throws JsonMappingException 
-	 * @throws JsonParseException 
-	 */
+   * Returns item iDs for unrated items, sorted by the recommendation algorithm
+   * 
+   * @param items items
+   * @return recommendations
+   * @throws IOException 
+   * @throws JsonProcessingException 
+   * @throws JsonMappingException 
+   * @throws JsonParseException 
+   */
 
-	public String[] getRecommendations(Value[] items) throws JsonParseException, JsonMappingException, JsonProcessingException, IOException {
-	  WekaRecommendationItem[] wekaItems = new WekaRecommendationItem[items.length];
+  public String[] getRecommendations(Value[] items) throws JsonParseException, JsonMappingException, JsonProcessingException, IOException {
+    WekaRecommendationItem[] wekaItems = new WekaRecommendationItem[items.length];
 	  
-	  wekaItems = Arrays.stream(items)
-	      .map(this::translateWekaRecommendationItem)
-	      .filter(Objects::nonNull)
-	      .toArray(WekaRecommendationItem[]::new);
+    wekaItems = Arrays.stream(items)
+    .map(this::translateWekaRecommendationItem)
+    .filter(Objects::nonNull)
+    .toArray(WekaRecommendationItem[]::new);
 	  
-	  String[] recommendations = wekaController.getRecommendations(wekaItems);
-	  return recommendations;
-	}
+    String[] recommendations = wekaController.getRecommendations(wekaItems);
+    return recommendations;
+  }
 	
-	private WekaRecommendationItem translateWekaRecommendationItem(Value item) {
-	  String id = item.getMember("id").asString();
-	  Value attributes = item.getMember("attributes");
-	  if(!attributes.hasArrayElements()){
-	    return null;
-	  }
-	  int size = (int) attributes.getArraySize();
-	  String[] attrs = new String[size];
-	  for(int i = 0; i < size; i++) {
-	    attrs[i] = attributes.getArrayElement(i).asString();
-	  }
-	  Value ratingValue = item.getMember("rating");
-	  Double rating = ratingValue.fitsInDouble()?ratingValue.asDouble():null;
-	  WekaRecommendationItem wekaItem = new WekaRecommendationItem();
-	  wekaItem.setId(id);
-	  wekaItem.setAttributes(attrs);
-	  wekaItem.setRating(rating);
-	  return wekaItem;
-	  
-	}
-
-  
+  private WekaRecommendationItem translateWekaRecommendationItem(Value item) {
+    String id = item.getMember("id").asString();
+    Value attributes = item.getMember("attributes");
+    if(!attributes.hasArrayElements()){
+      return null;
+    }
+    int size = (int) attributes.getArraySize();
+    String[] attrs = new String[size];
+    for(int i = 0; i < size; i++) {
+      attrs[i] = attributes.getArrayElement(i).asString();
+    }
+    Value ratingValue = item.getMember("rating");
+    Double rating = ratingValue.fitsInDouble()?ratingValue.asDouble():null;
+    WekaRecommendationItem wekaItem = new WekaRecommendationItem();
+    wekaItem.setId(id);
+    wekaItem.setAttributes(attrs);
+    wekaItem.setRating(rating);
+    return wekaItem;
+  }
 
 }
